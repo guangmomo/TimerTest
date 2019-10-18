39d9424828d2ac9c483f14472c2e2c39ff6094da
xuliwen <xuliwen@xuliwendeMacBook-Pro.local>
2019-10-18 10:54:47 +0800

    test

--- app/src/main/java/com/xlw/test/App.java
+++ app/src/main/java/com/xlw/test/App.java
@@ -4,7 +4,7 @@ import android.app.Application;
 import android.content.Context;
 
 /**
- * Created by xlw on 2017/3/31.
+ * Created by xlw on 2019/10/18.
  */
 
 public class App extends Application {
